a71cec1ef31500ca8e2734ad3c5feadb31d81a62
SladeSSmith <18912144596@163.com>
2016-12-08 21:37:01 +0800

    stage1:编写网络爬虫 step1:运行例子

---
+++ WebSpider/src/main/java/com/dingf/crawler/Test.java
@@ -0,0 +1,14 @@
+package com.dingf.crawler;
+
+public class Test {
+	public static void main( String[] args ){
+
+        String url = "http://www.baidu.com/";
+        String regex = "hidefocus.+?src=\"//(.+?)\"";
+        System.out.println(regex);
+        String result = HttpGetUtils.get(url);
+        System.out.println(result);
+        String src = RegexStringUtils.regexString(result, regex);
+        System.out.println(src);
+    }
+}
