ec0011d638e76ef8cbc71e6f3ae9a1824bc5734e
SladeSSmith <18912144596@163.com>
2016-12-10 11:42:15 +0800

    give up

--- WebSpider/src/main/java/com/dingf/crawler/Test.java
+++ WebSpider/src/main/java/com/dingf/crawler/Test.java
@@ -1,8 +1,7 @@
 package com.dingf.crawler;
 
 public class Test {
-	public static void main( String[] args ){
-
+	public static void main(String[] args) {			
         String url = "http://www.baidu.com/";
         String regex = "hidefocus.+?src=\"//(.+?)\"";
         System.out.println(regex);
